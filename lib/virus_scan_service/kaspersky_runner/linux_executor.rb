092548d2459fd6026c72be7805f37ad51e34d7de
Tomas Valent <equivalent@eq8.eu>
2015-02-03 16:47:48 +0000

    fix linux executor

--- lib/virus_scan_service/kaspersky_runner/linux_executor.rb
+++ lib/virus_scan_service/kaspersky_runner/linux_executor.rb
@@ -1,13 +1,14 @@
 module VirusScanService
   class KasperskyRunner
     class LinuxExecutor
+      # not tested on real production as I'm using windows VM for virus scans
       def scan(file_path, log_path)
-        system ['sudo',
+        system 'sudo',
           '/opt/kaspersky/kes4lwks/bin/kes4lwks-control',
           '--scan-file',
           file_path.to_s,
           ">>",
-          log_path.to_s]
+          log_path.to_s
       end
     end
   end
