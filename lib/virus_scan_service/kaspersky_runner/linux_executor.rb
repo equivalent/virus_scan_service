31ccf7c242a6c76cdbd8f1bcf215db1305068dc9
Tom Lord <tom.lord@comlaude.com>
2016-07-14 17:19:22 +0100

    Whitespace

--- lib/virus_scan_service/kaspersky_runner/linux_executor.rb
+++ lib/virus_scan_service/kaspersky_runner/linux_executor.rb
@@ -3,7 +3,7 @@ module VirusScanService
   class KasperskyRunner
     class LinuxExecutor
       def scan(file_path, log_path)
-	stdout, stderr, status = Open3.capture3(
+        stdout, stderr, status = Open3.capture3(
          "sudo /opt/kaspersky/kes4lwks/bin/kes4lwks-control --scan-file #{file_path.to_s}"
         )
         File.open(log_path.to_s, 'w') { |file| file.write(stdout) }
